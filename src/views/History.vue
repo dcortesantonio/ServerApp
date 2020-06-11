b5f297aa8988a8bd58619660a42f55b5715985b2
Daniela <dcortesantonio@gmail.com>
2020-06-11 12:31:54 -0500

    Configure History-API functionality

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,18 +4,14 @@
         <table class="table">
             <thead>
             <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">City</th>
+                <th scope="col">Domain</th>
+                <th scope="col">title</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="user in users" v-bind:key="user.id">
-                <th scope="row">{{user.id}}</th>
-                <td>{{user.name}}</td>
-                <td>{{user.email}}</td>
-                <td>{{user.address.city}}</td>
+            <tr v-for="domain in history" :key="domain.id">
+                <th scope="row">{{domain.domain}}</th>
+                <td>{{domain.infoserver.title}}</td>
             </tr>
             </tbody>
         </table>
@@ -23,21 +19,43 @@
 </template>
 
 <script>
-    import axios from 'axios';  export default {
-        name: 'Users',
+
+    const BASE_URL = 'http://localhost:8083';
+    import axios from 'axios';
+    export default {
+        name: 'History',
         data() {
             return {
-                users: null,
+                history: []
             };
         },
-        created: function() {
-            axios
-                .get('https://jsonplaceholder.typicode.com/users')
-                .then(res => {
-                    this.users = res.data;
-                })
+        created() {
+            axios.get(BASE_URL + '/history')
+                .then(response => {
+                    console.log("[Success] History ")
+                    console.log(response.data)
+                    this.history = response.data.items
+                    console.log(this.history)
+                }).catch(err => {
+                console.log("[Error] : Submit ", err)
+
+            });
+        },
+        methods: {
+            getlinkimg: function( value ){
+                console.log("[FUNCTION GETLINKIMG ] : ", value)
+                if (value == "")
+                return {
+                    link:"https://img.icons8.com/dotty/80/000000/id-not-verified.png"
+                };
+                else {
+                    console.log("[Tiene algo ] : ", value)
+                    return { link: ""+ String(value)};
+                }
+            }
         }
     }
+    // <a href="https://icons8.com/icon/79675/identificación-no-verificada">Identificación no verificada icon by Icons8</a>
 </script>
 
 <style>
